a848bee58ef97d78458edf3f9650885ba4003597
fergusohalloranOTD <fergusohalloran@onthedoor.net>
2025-03-19 20:27:06 +0000

    Adding plan

---
+++ main.go
@@ -0,0 +1,7 @@
+package main	
+
+func main(){
+	// Listen to "rounded_transactions" topic
+	// Get savings and perform the transaction to savings account
+	// Add to kafka topic "performed_transactions" userId, transactionAmount, timestamp
+}
